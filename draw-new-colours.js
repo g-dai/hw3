b11a687e858c883ee65b6314f39e90d86825df11
Grace <gdai@cca.edu>
2018-02-03 19:41:26 -0800

    Create draw-new-colours.js

---
+++ draw-new-colours.js
@@ -0,0 +1,28 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(211, 120, 120);
+  } else if (key == 'G') {
+    fill(120, 211, 120);
+  } else if (key == 'B') {
+    fill(120, 155, 211); 
+  } else if (key == 'Y') {
+		fill(255, 244, 183);
+	} else if (key == 'P') {
+		fill(209, 190, 229);
+	} else if (key == 'O') {
+		fill(247, 211, 165);
+	}
+}
