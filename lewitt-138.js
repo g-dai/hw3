c758c6b489172ca8370807f4128e887bef4d634a
Grace <gdai@cca.edu>
2018-02-03 23:38:04 -0800

    Update and rename lewitt-138 to lewitt-138.js

--- lewitt-138.js
+++ lewitt-138.js
@@ -4,6 +4,7 @@ function setup() {
 
 function draw() { 
   background(255);
+	stroke(193,193,193);
 	
 	noFill();
   for (var i = 30; i < height*2; i += 30) {
@@ -15,6 +16,14 @@ function draw() {
 	}
 	
 	for (var j = 15; j < height*2; j += 15) {
-		arc(200, j+10, width*1.5, height, PI, 0);
+		arc(200, j-255, width*1.5, height, 0, PI);
+	}
+	
+	for (var x = 0; x < width; x += 15) {
+		arc(x+20, height/2, 30, height, HALF_PI, PI*(3/2));
+	}
+	
+		for (var x = 0; x < width; x += 15) {
+		arc(x+20, height/2, 30, height, PI*(3/2), HALF_PI);
 	}
 }
